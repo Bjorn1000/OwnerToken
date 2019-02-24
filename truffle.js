b0de691cd70af8c3157229268ee121737d895d0e
Osbjorn Gulbranson <osbjorngulbranson@gmail.com>
2019-02-23 22:26:51 -0800

    token2

--- truffle.js
+++ truffle.js
@@ -1,3 +1,8 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infura_apikey = "0af6a67a5e6c4576a2b0132877dce63c";
+var mnemonic = "soon effort fire slab profit great borrow bacon stem wedding quit clean";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,14 +11,23 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: "7545",
-      network_id: "*" // match any network id
+      network_id: "*", // match any network id
+
     },
-    
+    /*
     rinkeby: {
       host: "localhost",
       port: 8545,
       network_id: 4,
       gas: 4700000
     }
+    */
+   ropsten: {
+    provider: function() {
+    return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey)
+    },
+    network_id: 3,
+    gas: 4698712
+  }
   }
 };
